895a7b604fb265bb40ecc8b6ff2ab57f17a7ee14
nimal <nimalrajm95@gmail.com>
2018-03-12 21:45:11 +0530

    reactapp

--- src/common/main.component.jsx
+++ src/common/main.component.jsx
@@ -8,14 +8,14 @@ class Main extends Component {
                 <nav className="navbar navbar-default">
                     <div className="container-fluid">
                         <div className="navbar-header">
-                            <a className="navbar-brand" href="#">Scotch Cars</a>
+                            <a className="navbar-brand" href="#">Popular Social Media Sites</a>
                         </div>
                         <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                             <ul className="nav navbar-nav">
                                 {/* Change from a to Link */}
                                 <li><Link to="/" activeclassname="active">Home</Link></li>
-                                <li><Link to="/cars" activeclassname="active">Cars</Link></li>
-                                <li><Link to="/about" activeclassname="active">About</Link></li>
+                                <li><Link to="/sites" activeclassname="active">Sites</Link></li>
+                                <li><Link to="/about" activeclassname="active">About Me</Link></li>
                             </ul>
                         </div>
                     </div>
